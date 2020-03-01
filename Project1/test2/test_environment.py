73af4d93ee1ee8366fce5c69d41fbd2e0a1d5999
Ivan <ivanryshov@mail.ru>
2020-03-01 16:20:26 +0300

    fixture
    1

--- Project1/test2/test_environment.py
+++ Project1/test2/test_environment.py
@@ -1,3 +1,7 @@
+from pytest import mark
+
+
+@mark.skip(reason="broken by deploy somenumber")
 def test_environment_is_qa(app_config):
     base_url = app_config.base_url
     port = app_config.app_port
@@ -9,3 +13,23 @@ def test_environment_is_dev(app_config):
     port = app_config.app_port
     assert base_url == 'https://mydev-env.com'
     assert port == 8080
+
+@mark.skip(reason="Not a staging environment")
+def test_environment_is_staging(app_config):
+    base_url = app_config.base_url
+    assert base_url == 'staging'
+
+@mark.xfail(reason="Env was not QA")
+def test_this_should_always_pass(app_config):
+    assert True
+
+@mark.xfail(reason="this feature should have been deprecated")
+def test_this_should_always_fail(app_config):
+    assert False
+
+
+@mark.skip
+def test_this_needs_work(app_config):
+    assert False
+
+
