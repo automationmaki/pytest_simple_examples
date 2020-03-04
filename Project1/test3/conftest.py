a0bd67ca019a964bfad707fd13acc61a020d5d00
Ivan <ivanryshov@mail.ru>
2020-03-04 17:43:07 +0300

    fx

---
+++ Project1/test3/conftest.py
@@ -0,0 +1,25 @@
+from pytest import fixture
+import json
+from selenium import webdriver
+
+
+data_path = 'test_data.json'
+
+def load_test_json(path):
+    with open(path) as data_file:
+        data = json.load(data_file)
+        return data 
+
+
+@fixture(params=[webdriver.Chrome, webdriver.Firefox, webdriver.Edge])
+def browser(request):
+    driver = request.param
+    drvr = driver()
+    yield drvr 
+    drvr.quit()
+
+
+@fixture(params=load_test_data(data_path))
+def tv_brand(request):
+    data = request.param
+    return data
